0b6f7c184d3b595c07c7678cc247ccd7653fe379
RuslanUchan <ruslanuchan97@gmail.com>
2023-03-22 23:50:32 +0700

    passing addition test - add `run` method that accept and returns `State` - list all numbers (instead of `.isDigit`) in `case` to get numbers

--- src/main/scala/Forth.scala
+++ src/main/scala/Forth.scala
@@ -11,14 +11,29 @@ class State extends ForthEvaluatorState {
 
 class Forth extends ForthEvaluator {
   def eval(text: String): Either[ForthError, ForthEvaluatorState] = {
-    val inputs = text
+    text
       .split(" ")
-      .map(_.toInt)
-      .foldLeft[State](new State)((s, i) => {
-        s.stack.push(i)
-        s
+      .foldLeft[Either[ForthError, State]](Right(new State))((state, str) => {
+        state match {
+          case Right(s) => run(s, str)
+          case Left(s)  => state
+        }
       })
+  }
 
-    Right(inputs)
+  def run(state: State, str: String): Either[ForthError, State] = {
+    str match {
+      case "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" => {
+        state.stack.push(str.toInt)
+        Right(state)
+      }
+      case "+" => {
+        val result = state.stack.take(2).sum
+        val drop = state.stack.drop(2)
+        state.stack = drop.push(result)
+        Right(state)
+      }
+      case _ => Left(ForthError.UnknownWord)
+    }
   }
 }
